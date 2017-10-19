5b12104340c3a9b603e7bdf0fac0385fd34d2da9
Teijo Laine <teijo@aropupu.fi>
2017-10-18 22:39:58 -0400

    Materialize root matches correctly

--- src/jquery.bracket.ts
+++ src/jquery.bracket.ts
@@ -382,12 +382,13 @@
   }
 
   const endOfBranch = () => { throw new EndOfBranchException(); };
-  const winnerMatchSources = (teams: [any, any][], m: number) => (): [MatchSource, MatchSource] => [
-    {source: () => new TeamBlock(endOfBranch,
-        teams[m][0], Option.of(Order.first()), Option.of<number>(m * 2), Score.empty())},
-    {source: () => new TeamBlock(endOfBranch,
-        teams[m][1], Option.of(Order.second()), Option.of<number>(m * 2 + 1), Score.empty())}
-  ];
+  const rootMatchSources = (teams: [any, any][], m: number) => (): [MatchSource, MatchSource] => {
+    const teamA = new TeamBlock(endOfBranch, teams[m][0],
+      Option.of(Order.first()), Option.of<number>(m * 2), Score.empty());
+    const teamB = new TeamBlock(endOfBranch, teams[m][1],
+      Option.of(Order.second()), Option.of<number>(m * 2 + 1), Score.empty());
+    return [{source: () => teamA}, {source: () => teamB}];
+  };
 
   const winnerAlignment = (match: Match, skipConsolationRound: boolean) => (tC: JQuery) => {
     tC.css('top', '');
@@ -410,7 +411,7 @@
       round = winners.addRound(Option.empty());
 
       for (let m = 0; m < matchCount; m += 1) {
-        const teamCb = (r === 0) ? winnerMatchSources(teams, m) : null;
+        const teamCb = (r === 0) ? rootMatchSources(teams, m) : null;
         if (!(r === roundCount - 1 && isSingleElimination) && !(r === roundCount - 1 && skipGrandFinalComeback)) {
           round.addMatch(teamCb, Option.empty());
         }
